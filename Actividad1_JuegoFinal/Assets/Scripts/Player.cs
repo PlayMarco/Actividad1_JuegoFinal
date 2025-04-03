57345d6cc5631d2ad34d3fc78c8cbab768bae988
PlayMarco <marcofloresbowen@hotmail.com>
2025-04-02 22:20:16 -0500

    Movimiento Personaje

--- Actividad1_JuegoFinal/Assets/Scripts/Player.cs
+++ Actividad1_JuegoFinal/Assets/Scripts/Player.cs
@@ -2,31 +2,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody rb;
     private float inputH;
     private float inputV;
-    [SerializeField] private float fuerzaSalto;
-    [SerializeField] private float fuerzaMovimiento;
+    private CharacterController characterController;
+    [SerializeField] private float velocidadMov;
+    [SerializeField] private Transform camara;
+    private Animator animator;
+    private Vector3 direccionMov;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputH = Input.GetAxisRaw("Horizontal");
-        inputV = Input.GetAxisRaw("Vertical");
-        if(Input.GetKeyDown(KeyCode.Space))
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+        direccionMov = (camara.forward * inputV + camara.right * inputH).normalized;
+        direccionMov.y = 0;
+        characterController.Move(direccionMov * velocidadMov * Time.deltaTime);
+        animator.SetFloat ("velocidad", characterController.velocity.magnitude);
+        if (inputH != 0 || inputV != 0) 
         {
-            rb.AddForce(Vector3.up *  fuerzaSalto, ForceMode.Impulse);
+            RotarHaciaDestino(); 
         }
     }
-
-    //Fisicas CONTINUAS
-    private void FixedUpdate()
-    {
-        rb.AddForce(new Vector3(inputH, 0, inputV) * fuerzaMovimiento, ForceMode.Force);
+    private void RotarHaciaDestino() 
+    { 
+        Quaternion rotacionObjetivo = Quaternion.LookRotation(direccionMov);
+        transform.rotation = rotacionObjetivo;
     }
 }

a87a9adddf5c4fb1634f6e8190b41620cec520de
PlayMarco <marcofloresbowen@hotmail.com>
2025-04-11 21:31:02 -0500

    Retoques

--- Actividad1_JuegoFinal/Assets/ScriptableObjects/InputManagerSO.cs
+++ Actividad1_JuegoFinal/Assets/ScriptableObjects/InputManagerSO.cs
@@ -2,21 +2,31 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using System;
 
 [CreateAssetMenu(menuName = "InputManager")]
 public class InputManagerSO : ScriptableObject
 {
     InputSystem_Actions controles;
+    public event Action OnSalto;
+    public event Action<Vector2> OnMover;
     private void OnEnable()
     {
         controles = new InputSystem_Actions();
         controles.Player.Enable();
         controles.Player.Jump.started += Saltar;
-        Debug.Log("Estoy Listo");
+        controles.Player.Move.performed += Mover;
+        controles.Player.Move.canceled += Mover;
     }
 
-    private void Saltar(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    private void Mover(InputAction.CallbackContext ctx)
     {
-        
+        OnMover?.Invoke(ctx.ReadValue<Vector2>());
+    }
+
+    private void Saltar(InputAction.CallbackContext ctx)
+    {
+        OnSalto?.Invoke();
     }
 }
--- Actividad1_JuegoFinal/Assets/Scripts/Player.cs
+++ Actividad1_JuegoFinal/Assets/Scripts/Player.cs
@@ -1,14 +1,44 @@
+using System.Collections;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float inputH;
-    private float inputV;
     private CharacterController characterController;
     [SerializeField] private float velocidadMov;
     [SerializeField] private Transform camara;
+    [SerializeField] private InputManagerSO inputManager;
+    [SerializeField] private float factorGravedad;
+    [SerializeField] private Transform pies;
+    [SerializeField] private float radioDeteccion;
+    [SerializeField] private LayerMask queEsSuelo;
+    [SerializeField] private float alturaSalto;
     private Animator animator;
     private Vector3 direccionMov;
+    private Vector3 direccionInput;
+    private Vector3 velocidadVertical;
+
+    private void OnEnable()
+    {
+        inputManager.OnSalto += Saltar;
+        inputManager.OnMover += Mover;
+    }
+
+    private void Mover(Vector2 ctx)
+    {
+        direccionInput = new Vector3 (ctx.x, 0, ctx.y);
+    }
+
+    private void Saltar()
+    {
+        if (EstoyEnElSuelo()) 
+        {
+            velocidadVertical.y = Mathf.Sqrt(-2 * factorGravedad * alturaSalto);
+            animator.SetTrigger("Jump");
+        }
+    }
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,17 +50,35 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        inputH = Input.GetAxis("Horizontal");
-        inputV = Input.GetAxis("Vertical");
-        direccionMov = (camara.forward * inputV + camara.right * inputH).normalized;
+        direccionMov = camara.forward * direccionInput.z + camara.right * direccionInput.x;
         direccionMov.y = 0;
         characterController.Move(direccionMov * velocidadMov * Time.deltaTime);
         animator.SetFloat ("velocidad", characterController.velocity.magnitude);
-        if (inputH != 0 || inputV != 0) 
+        if (direccionMov.sqrMagnitude > 0)
         {
-            RotarHaciaDestino(); 
+            RotarHaciaDestino();
         }
+
+        if (EstoyEnElSuelo() && velocidadVertical.y < 0) 
+        {
+            velocidadVertical.y = 0;
+            animator.ResetTrigger("Jump");
+        }
+
+        AplicarGravedad();
+
     }
+    private void AplicarGravedad()
+    {
+        velocidadVertical.y += factorGravedad * Time.deltaTime;
+        characterController.Move(velocidadVertical * Time.deltaTime);
+    }
+
+    private bool EstoyEnElSuelo()
+    {
+       return Physics.CheckSphere(pies.position, radioDeteccion, queEsSuelo);
+    }
+
     private void RotarHaciaDestino() 
     { 
         Quaternion rotacionObjetivo = Quaternion.LookRotation(direccionMov);
